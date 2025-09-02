40970e75a7921ad71c4a762c60e6db12e69fe2f7
brendon-z <brendon.zhao@gmail.com>
2025-09-02 14:58:18 +1000

    opponent choice implementation using enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+use enums::OpponentChoice;
+
+pub mod enums;
+
+fn choose_opponent() -> OpponentChoice {
+    loop {
+        println!("Do you want to play against a human or a computer (NOT IMPLEMENTED YET)?");
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let answer = input.trim();
+                match answer {
+                    "human" => {return OpponentChoice::Human},
+                    "computer" => {
+                        println!("Computer opponents are not implemented yet.");
+                    },
+                    _ => {println!("Invalid option!")}
+                }
+            },
+            Err(_) => {
+                println!("Failed to read input, try again.");
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Battleship, implemented in Rust.");
+    let opponent_choice = choose_opponent();
+    println!("You have chosen to battle a {:?}", opponent_choice);
 }
